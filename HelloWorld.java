557f5bb83cc8ce67be383d661499f8b8ec03e1f0
Dhiraj Gupta <dhiraj3@gmail.com>
2015-01-23 13:00:16 -0800

    Important hot fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
     }
+		// Fixing this critical via hot fix
 }
 
 // This line is added in the master branch
