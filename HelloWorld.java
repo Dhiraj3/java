c2258df6a2638b0482934828a3c17208e660aa8e
Dhiraj Gupta <dhiraj3@gmail.com>
2015-01-23 13:01:09 -0800

    Important hot fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
+		//HOT FIX
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
     }
